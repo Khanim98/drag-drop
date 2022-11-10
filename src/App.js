a4d0f7f05f9302f295e0233b929cb8d3a12074b1
Xanim <xanim.imam@mail.ru>
2022-11-10 22:37:28 +0400

    fixed twice adding bug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  const [boards, setBoards] = React.useState([
+    {id: 1, title: 'Todo', items: [{id:1, title: 'Designing'}, {id: 2, title: 'Change Color'}, {id: 3, title: 'Make Testing'}]},
+    {id: 2, title: 'Test', items: [{id:4, title: 'Creating Menu'}, {id: 5, title: 'Creating Search'}, {id: 6, title: 'Create button'}]},
+    {id: 3, title: 'Done', items: [{id:7, title: 'Fetching Data'}, {id: 8, title: 'Filtering Data'}, {id: 9, title: 'Adding New data'}]},
+  ])
+
+  const [ currentBoard, setCurrentBoard ] = React.useState(null)
+  const [ currentItem, setCurrentItem ] = React.useState(null)
+
+  function dragOverHandler(e) {
+    e.preventDefault()
+    if(e.target.className == 'item'){
+      e.target.style.boxShadow = '0 2px 3px gray'
+    }
+  }
+
+  function ondragleaveHandler(e) {
+    e.target.style.boxShadow = 'none'
+
+  }
+
+  function dragStartHandler(e, board, item) {
+    setCurrentBoard(board)
+    setCurrentItem(item)
+  }
+
+  function dragEndHandler(e) {
+    e.target.style.boxShadow = 'none'
+  }
+
+  function dropHandler(e, board, item) {
+    e.preventDefault();
+    e.target.style.boxShadow = 'none'
+  }
+
+  function dropCardHandler(e, board) {
+    e.preventDefault();
+    board.items.push(currentItem)
+    const currentIndex = currentBoard.items.indexOf(currentItem);
+    currentBoard.items.splice(currentIndex, 1);
+    setBoards(boards.map(b => {
+      if(b.id === board.id){
+        return board
+      }
+      if(b.id === currentBoard.id){
+        return currentBoard
+      }
+      return b
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      {boards.map(board =>
+        <div
+            className="board"
+            onDragOver={(e) => dragOverHandler(e)}
+            onDrop={(e) => dropCardHandler(e, board)}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="board__title">{board.title}</div>
+          {board.items.map(item =>
+            <div
+                className="item"
+                draggable={true}
+                onDragOver={(e) => dragOverHandler(e)}
+                onDragLeave={e => ondragleaveHandler(e)}
+                onDragStart={(e) => dragStartHandler(e, board, item)}
+                onDragEnd={(e) => dragEndHandler(e)}
+                onDrop={(e) => dropHandler(e, board, item)}
+            >
+              {item.title}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
